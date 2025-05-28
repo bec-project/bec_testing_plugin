66ac6c9eda5430d5e5ff96571033edd66f8172dd
perl_d <david.perl@psi.ch>
2025-05-28 19:44:36 +0200

    fix: add kwargs to widget plugins

--- bec_testing_plugin/bec_widgets/widgets/ExampleWidgetPlugin/ExampleWidgetPlugin.py
+++ bec_testing_plugin/bec_widgets/widgets/ExampleWidgetPlugin/ExampleWidgetPlugin.py
@@ -1,11 +1,11 @@
+from bec_widgets.utils.bec_widget import BECWidget
 from qtpy.QtWidgets import QWidget
 
-from bec_widgets.utils.bec_widget import BECWidget
-
-
-from bec_testing_plugin.bec_widgets.widgets.ExampleWidgetPlugin.ExampleWidgetPlugin_ui import Ui_Examplewidgetplugin
+from bec_testing_plugin.bec_widgets.widgets.ExampleWidgetPlugin.ExampleWidgetPlugin_ui import (
+    Ui_Examplewidgetplugin,
+)
 
 
 class Examplewidgetplugin(BECWidget, QWidget, Ui_Examplewidgetplugin):
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent=parent, **kwargs)
--- bec_testing_plugin/bec_widgets/widgets/NoUiWidgetPlugin/NoUiWidgetPlugin.py
+++ bec_testing_plugin/bec_widgets/widgets/NoUiWidgetPlugin/NoUiWidgetPlugin.py
@@ -1,9 +1,7 @@
-from qtpy.QtWidgets import QWidget
-
 from bec_widgets.utils.bec_widget import BECWidget
-
+from qtpy.QtWidgets import QWidget
 
 
 class Nouiwidgetplugin(BECWidget, QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent=parent, **kwargs)
