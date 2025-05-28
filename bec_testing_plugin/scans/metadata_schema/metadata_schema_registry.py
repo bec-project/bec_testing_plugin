2a95393b1df111fe654f61bd635011b13fba692a
perl_d <david.perl@psi.ch>
2025-05-28 16:35:00 +0200

    feat: add a custom scan and metadata schema

--- bec_testing_plugin/scans/metadata_schema/metadata_schema_registry.py
+++ bec_testing_plugin/scans/metadata_schema/metadata_schema_registry.py
@@ -1,10 +1,11 @@
 # from .metadata_schema_template import ExampleSchema
+from .custom_test_scan_schema import CustomScanSchema
 
 METADATA_SCHEMA_REGISTRY = {
     # Add models which should be used to validate scan metadata here.
     # Make a model according to the template, and import it as above
     # Then associate it with a scan like so:
-    # "example_scan": ExampleSchema
+    "custom_testing_scan": CustomScanSchema
 }
 
 # Define a default schema type which should be used as the fallback for everything:
