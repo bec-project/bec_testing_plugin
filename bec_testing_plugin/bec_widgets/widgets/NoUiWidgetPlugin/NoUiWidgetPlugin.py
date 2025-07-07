15112defea1054c00283276258402c646feebb89
David Perl <david.perl@psi.ch>
2025-07-07 13:33:17 +0200

    update plugin version

--- bec_testing_plugin/bec_widgets/widgets/NoUiWidgetPlugin/NoUiWidgetPlugin.py
+++ bec_testing_plugin/bec_widgets/widgets/NoUiWidgetPlugin/NoUiWidgetPlugin.py
@@ -5,3 +5,13 @@ from qtpy.QtWidgets import QWidget
 class Nouiwidgetplugin(BECWidget, QWidget):
     def __init__(self, parent=None, **kwargs):
         super().__init__(parent=parent, **kwargs)
+
+if __name__ == "__main__":
+    import sys
+
+    from qtpy.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    widget = Nouiwidgetplugin()
+    widget.show()
+    sys.exit(app.exec())
